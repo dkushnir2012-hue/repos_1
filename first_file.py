4b48ca8050bd32a5bee3174473b9082e048e6b95
“John <john@email.com>
2026-04-20 11:16:54 +0300

    add poems

---
+++ first_file.py
@@ -0,0 +1,19 @@
+# city = "Одеса"
+# country = "Україна"
+# message = "Мені дуже подобається це місто."
+#
+# print(city)
+# print(country)
+# print(message)
+#
+# dialogue = """- Привіт;
+# - Ти сьогодні приїдеш до 'Одеси'?;
+# - Так сьогодні вже буду там;"""
+
+# print(dialogue)
+
+# um = "☔"
+um = "\U00002614"
+print (um)
+print (ord(um))
+print (chr(9748))
---
+++ main.py
@@ -0,0 +1,16 @@
+import poems
+import sing
+
+name = "Іван Петренко"
+
+print(sing.SIGN.format(name))
+wishes = "Більше практики\nЦікаві проекти"
+print(wishes)
+
+stars = "**************************************************************************"
+
+print(stars)
+print(poems.POEM1)
+print(stars)
+print(poems.POEM2)
+print(stars)
